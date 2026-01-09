3fc437f2227c9d4b49b011f7c52556944c74b674
Lapin Dmytro <lapin_dm@yahoo.com>
2026-01-09 21:26:47 +0100

    i have added a basic model for a ticket, created some file structure for a project.

---
+++ Models/Ticketmodel.cs
@@ -0,0 +1,14 @@
+public class Ticket {
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Description { get; set; } = "";
+    public TicketStatus Status { get; set; }
+    public int Priority { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
+public enum TicketStatus {
+    Open,
+    InProgress,
+    Done
+}
