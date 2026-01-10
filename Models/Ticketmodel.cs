d2368f498a6fb9c0be9e2bb46802732caeb98173
Lapin Dmytro <lapin_dm@yahoo.com>
2026-01-10 23:03:49 +0100

    i've added just a plain endpoints scheme.

--- Models/Ticketmodel.cs
+++ Models/Ticketmodel.cs
@@ -1,14 +1,17 @@
-public class Ticket {
+namespace TicketManagmentSystem.Models
+{
+    public class Ticket {
     public int Id { get; set; }
     public string Title { get; set; } = "";
     public string Description { get; set; } = "";
     public TicketStatus Status { get; set; }
     public int Priority { get; set; }
     public DateTime CreatedAt { get; set; }
-}
+    }
 
-public enum TicketStatus {
-    Open,
-    InProgress,
-    Done
+    public enum TicketStatus {
+        Open,
+        InProgress,
+        Done
+    }
 }
