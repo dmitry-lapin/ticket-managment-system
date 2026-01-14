1a7c6e05966c456e2f067f08ebf8f0d2983fc9ec
Lapin Dmytro <lapin_dm@yahoo.com>
2026-01-14 20:27:18 +0100

    were updated Dto's policies, improved controller logic, aswell as TicketService section.

--- Dtos/TicketDtos/UpdateTicketDto.cs
+++ Dtos/TicketDtos/UpdateTicketDto.cs
@@ -1,9 +1,13 @@
 using TicketManagmentSystem.Models;
+using System.ComponentModel.DataAnnotations;
 
 public class UpdateTicketDto
 {
+    [MaxLength(100)]
     public string? Title { get; set; }
+    [MaxLength(500)]
     public string? Description { get; set; }
     public TicketStatus? Status { get; set; }
+    [Range(1,5)]
     public int? Priority { get; set; }
 }
