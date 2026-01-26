30fa6efa93eb3db67b0a4ab354353edb406223b0
Lapin Dmitry <lapin_dm@yahoo.com>
2026-01-26 23:21:41 +0100

    i am implementing ci/cd workflow to my project.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,12 +1,18 @@
 using Microsoft.EntityFrameworkCore;
-
+using System.Text.Json.Serialization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new JsonStringEnumConverter()
+        );
+    });
 
 builder.Services.AddCors(options =>
 {
